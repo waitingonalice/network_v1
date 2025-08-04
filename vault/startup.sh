bef8bba078d812e48edcebb961a1061a61d5071d
Wilson <fcsie55@gmail.com>
2025-08-04 20:17:59 +0800

    add vault warden startup check

--- vault/startup.sh
+++ vault/startup.sh
@@ -1,6 +1,6 @@
 check_docker_startup(){
   while true; do
-    if docker ps > /dev/null 2>&1; then
+    if docker ps | grep "8080" > /dev/null 2>&1; then
         tailscale serve http://127.0.1:8080
         break
     else
