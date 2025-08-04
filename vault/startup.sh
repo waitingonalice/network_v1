61a2ec6697e2cf06ae41840ad3927b524ef7767a
Wilson <fcsie55@gmail.com>
2025-08-04 20:22:04 +0800

    fix ip addr

--- vault/startup.sh
+++ vault/startup.sh
@@ -1,7 +1,7 @@
 check_docker_startup(){
   while true; do
     if docker ps | grep "8080" > /dev/null 2>&1; then
-        tailscale serve http://127.0.1:8080
+        tailscale serve http://127.0.0.1:8080
         break
     else
         sleep 5
